9d679b120663d1dbb4c7e2ca1ea116672c7789c5
EC2 Default User <ec2-user@ip-172-31-49-185.ec2.internal>
2021-11-18 17:02:28 +0000

    updates

--- Week-10-aa/wa10b.js
+++ Week-10-aa/wa10b.js
@@ -18,18 +18,24 @@ db_credentials.password = process.env.AWSRDS_PW;
 db_credentials.port = 5432;
 
 var addressesForDb = content;
-async.eachSeries(addressesForDb, function(value, callback) {
+async.eachSeries(
+    
+
+    addressesForDb, function(value, callback) {
     const client = new Client(db_credentials);
     client.connect();
     // var thisQuery = "INSERT INTO meeting_loc(name,location,lat,lng,postcode,floor,wheelchair,groupid) VALUES ($$"+ value.name +"$$ , $$" + value.location + "$$ , $$"+ value.lat + "$$ , $$" + value.lng + "$$ , $$"+ value.postcode + "$$ , $$" + value.floor + "$$, $$" + value.wheelChair +"$$, $$" + value.group +"$$ );";
 
-    var thisQuery = "INSERT INTO aa_meeting_database(address,lat,long,zipCode,zone,venue,roomDetail,directionDetail,groupName,wheelchairAccess,miscDetails,day,trueStartTime,trueEndTime,meetingType,specialInerest) VALUES ($$" + value.thisMeeting.address + "$$, $$" + value.latLong.lat + "$$, $$" + value.latLong.long + "$$, $$" + value.thisMeeting.zipCode + "$$, $$" + value.thisMeeting.zone + "$$, $$" + value.thisMeeting.venue + "$$, $$" + value.thisMeeting.roomDetail + "$$, $$" + value.thisMeeting.directionDetail + "$$, $$" + value.thisMeeting.groupName + "$$, $$" + value.thisMeeting.wheelchairAccess + "$$, $$" + value.thisMeeting.miscDetails + "$$, $$" + value.thisMeeting.meetingDetails.day[0] + "$$, $$" + value.thisMeeting.meetingDetails.trueStartTime[0] + "$$, $$" + value.thisMeeting.meetingDetails.trueEndTime[0] + "$$, $$" + value.thisMeeting.meetingDetails.meetingType[0] + "$$, $$" + value.thisMeeting.meetingDetails.specialInerest[0] + "$$);";
+{
+    for (let i=0; i<addressesForDb.length; i++)
+    var thisQuery = "INSERT INTO aa_meeting_database(address,lat,long,zipCode,zone,venue,roomDetail,directionDetail,groupName,wheelchairAccess,miscDetails,day,trueStartTime,trueEndTime,meetingType,specialInerest) VALUES ($$" + value.thisMeeting.address + "$$, $$" + value.latLong.lat + "$$, $$" + value.latLong.long + "$$, $$" + value.thisMeeting.zipCode + "$$, $$" + value.thisMeeting.zone + "$$, $$" + value.thisMeeting.venue + "$$, $$" + value.thisMeeting.roomDetail + "$$, $$" + value.thisMeeting.directionDetail + "$$, $$" + value.thisMeeting.groupName + "$$, $$" + value.thisMeeting.wheelchairAccess + "$$, $$" + value.thisMeeting.miscDetails + "$$, $$" + value.thisMeeting.meetingDetails.day + "$$, $$" + value.thisMeeting.meetingDetails.trueStartTime + "$$, $$" + value.thisMeeting.meetingDetails.trueEndTime + "$$, $$" + value.thisMeeting.meetingDetails.meetingType + "$$, $$" + value.thisMeeting.meetingDetails.specialInerest + "$$);";
+    // var thisQuery = "INSERT INTO aa_meeting_database(address,lat,long,zipCode,zone,venue,roomDetail,directionDetail,groupName,wheelchairAccess,miscDetails) VALUES ($$" + value.thisMeeting.address + "$$, $$" + value.latLong.lat + "$$, $$" + value.latLong.long + "$$, $$" + value.thisMeeting.zipCode + "$$, $$" + value.thisMeeting.zone + "$$, $$" + value.thisMeeting.venue + "$$, $$" + value.thisMeeting.roomDetail + "$$, $$" + value.thisMeeting.directionDetail + "$$, $$" + value.thisMeeting.groupName + "$$, $$" + value.thisMeeting.wheelchairAccess + "$$, $$" + value.thisMeeting.miscDetails + "$$);";
     client.query(thisQuery, (err, res) => {
         console.log(err, res);
         client.end();
     });
     setTimeout(callback, 1000); 
-}); 
+}}); 
 
 //try to troubleshoot by inserting one column at a time
 
